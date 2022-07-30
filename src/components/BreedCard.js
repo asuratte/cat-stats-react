3936c7acc93d83b7067a246b19aaa3fb2bef5cc6
Amber Suratte <as00193@my.westga.edu>
2022-07-29 23:02:03 -0500

    Added select box for breed in BreedCard and added change handler function to update the breed upon selection.

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -34,115 +34,131 @@ const BreedCard = () => {
         }
     }
 
+    const handleChange = event => {
+        setCurrentBreedNumber(event.target.value)
+    };
+
     if (breedsList) {
         return (
-            <div className="breed-card-wrapper d-flex justify-content-center">
-                <div class="button-wrap">
-                    <Button onClick={getPrevious} className="btn btn-primary">Previous</Button>
+            <div className="container">
+                <div class="breeds-select text-center mb-4">
+                    <strong>Select a Breed: </strong>
+                    <select onChange={handleChange} name="breeds">
+                        {breedsList.map((breed, index) => (
+                            <option key={index} value={index}>
+                                {breed.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
-                <div className="breed-card text-center mx-5">
-                    <div className="name">
-                        <h3>{ breedsList[currentBreedNumber].name }</h3>
+                <div className="breed-card-wrapper d-flex justify-content-center">
+                    <div className="button-wrap">
+                        <Button onClick={getPrevious} className="btn btn-primary">Previous</Button>
                     </div>
-                    <div className="description">
-                        <p>{ breedsList[currentBreedNumber].description }</p>
-                    </div>
-                    <div className="image">
-                        <img src={ breedsList[currentBreedNumber].image.url } alt={ breedsList[currentBreedNumber].name } />
-                    </div>
-                    <div className="temperament">
-                        <strong>Temperament</strong>
-                        <p>{ breedsList[currentBreedNumber].temperament }</p>
-                    </div>
-                    <div className="origin">
-                        <strong>Place of Origin</strong>
-                        <p>{ breedsList[currentBreedNumber].origin }</p>
-                    </div>
-                    <div className="weight">
-                        <strong>Weight</strong>
-                        <p>{ breedsList[currentBreedNumber].weight.imperial } lbs</p>
-                    </div>
-                    <div className="attributes-points">
-                        <ul>
-                            <li>Affection +{ breedsList[currentBreedNumber].affection_level }</li>
-                            <li>Energy +{ breedsList[currentBreedNumber].energy_level }</li>
-                            <li>Vocalization +{ breedsList[currentBreedNumber].vocalisation }</li>
-                            <li>Intelligence +{ breedsList[currentBreedNumber].intelligence }</li>
-                            <li>Shedding +{ breedsList[currentBreedNumber].shedding_level }</li>
-                            <li>Grooming +{ breedsList[currentBreedNumber].grooming }</li>
-                            <li>Dog Friendly +{ breedsList[currentBreedNumber].dog_friendly }</li>
-                            <li>Child Friendly +{ breedsList[currentBreedNumber].child_friendly }</li>
-                        </ul>
-                    </div>
-                    <div className="attributes-yes-no">
-                        <ul>
-                            <li>
-                                {
-                                    (breedsList[currentBreedNumber].lap)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Lap Cat</li>
-                             <li>
-                                {
-                                    (breedsList[currentBreedNumber].rare)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Rare</li>
-                             <li>
-                                {
-                                    (breedsList[currentBreedNumber].suppressed_tail)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Suppressed Tail</li>
-                             <li>
-                                {
-                                    (breedsList[currentBreedNumber].short_legs)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Short Legs</li>
-                             <li>
-                                {
-                                    (breedsList[currentBreedNumber].hypoallergenic)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Hypoallergenic</li>
-                             <li>
-                                {
-                                    (breedsList[currentBreedNumber].experimental)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Experimental</li>
-                             <li>
-                                {
-                                    (breedsList[currentBreedNumber].natural)
-                                    ? <span className="attribute-icon yes"><CheckLg /></span>
-                                    : <span className="attribute-icon no"><XLg /></span>
-                                }
-                             Natural</li>
-                        </ul>
-                    </div>
-                    { (breedsList[currentBreedNumber].wikipedia_url || breedsList[currentBreedNumber].vetstreet_url || breedsList[currentBreedNumber].vcahospitals_url) && 
-                        <div className="learn-more">
-                            <strong>Learn More</strong>
-                            { breedsList[currentBreedNumber].wikipedia_url && <p><a className="link-primary" href={ breedsList[currentBreedNumber].wikipedia_url } target="_blank" rel="noopener noreferrer">Wikipedia</a> <BoxArrowUpRight/></p> }  
-                            { breedsList[currentBreedNumber].vetstreet_url && <p><a className="link-primary" href={ breedsList[currentBreedNumber].vetstreet_url } target="_blank" rel="noopener noreferrer">Vetstreet</a> <BoxArrowUpRight/></p> }  
-                            { breedsList[currentBreedNumber].vcahospitals_url && <p><a className="link-primary" href={ breedsList[currentBreedNumber].vcahospitals_url } target="_blank" rel="noopener noreferrer">VCA Animal Hospitals</a> <BoxArrowUpRight/></p> }  
+                    <div className="breed-card text-center mx-5">
+                        <div className="name">
+                            <h3>{breedsList[currentBreedNumber].name}</h3>
                         </div>
-                    }
-                </div>
-                <div class="button-wrap">
-                    <Button onClick={getNext} className="btn btn-primary">Next</Button>
+                        <div className="description">
+                            <p>{breedsList[currentBreedNumber].description}</p>
+                        </div>
+                        <div className="image">
+                            <img src={breedsList[currentBreedNumber].image.url} alt={breedsList[currentBreedNumber].name} />
+                        </div>
+                        <div className="temperament">
+                            <strong>Temperament</strong>
+                            <p>{breedsList[currentBreedNumber].temperament}</p>
+                        </div>
+                        <div className="origin">
+                            <strong>Place of Origin</strong>
+                            <p>{breedsList[currentBreedNumber].origin}</p>
+                        </div>
+                        <div className="weight">
+                            <strong>Weight</strong>
+                            <p>{breedsList[currentBreedNumber].weight.imperial} lbs</p>
+                        </div>
+                        <div className="attributes-points">
+                            <ul>
+                                <li>Affection +{breedsList[currentBreedNumber].affection_level}</li>
+                                <li>Energy +{breedsList[currentBreedNumber].energy_level}</li>
+                                <li>Vocalization +{breedsList[currentBreedNumber].vocalisation}</li>
+                                <li>Intelligence +{breedsList[currentBreedNumber].intelligence}</li>
+                                <li>Shedding +{breedsList[currentBreedNumber].shedding_level}</li>
+                                <li>Grooming +{breedsList[currentBreedNumber].grooming}</li>
+                                <li>Dog Friendly +{breedsList[currentBreedNumber].dog_friendly}</li>
+                                <li>Child Friendly +{breedsList[currentBreedNumber].child_friendly}</li>
+                            </ul>
+                        </div>
+                        <div className="attributes-yes-no">
+                            <ul>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].lap)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Lap Cat</li>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].rare)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Rare</li>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].suppressed_tail)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Suppressed Tail</li>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].short_legs)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Short Legs</li>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].hypoallergenic)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Hypoallergenic</li>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].experimental)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Experimental</li>
+                                <li>
+                                    {
+                                        (breedsList[currentBreedNumber].natural)
+                                            ? <span className="attribute-icon yes"><CheckLg /></span>
+                                            : <span className="attribute-icon no"><XLg /></span>
+                                    }
+                                    Natural</li>
+                            </ul>
+                        </div>
+                        {(breedsList[currentBreedNumber].wikipedia_url || breedsList[currentBreedNumber].vetstreet_url || breedsList[currentBreedNumber].vcahospitals_url) &&
+                            <div className="learn-more">
+                                <strong>Learn More</strong>
+                                {breedsList[currentBreedNumber].wikipedia_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].wikipedia_url} target="_blank" rel="noopener noreferrer">Wikipedia</a> <BoxArrowUpRight /></p>}
+                                {breedsList[currentBreedNumber].vetstreet_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vetstreet_url} target="_blank" rel="noopener noreferrer">Vetstreet</a> <BoxArrowUpRight /></p>}
+                                {breedsList[currentBreedNumber].vcahospitals_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vcahospitals_url} target="_blank" rel="noopener noreferrer">VCA Animal Hospitals</a> <BoxArrowUpRight /></p>}
+                            </div>
+                        }
+                    </div>
+                    <div className="button-wrap">
+                        <Button onClick={getNext} className="btn btn-primary">Next</Button>
+                    </div>
                 </div>
             </div>
         )
     }
-    
+
 }
 
 export default BreedCard
