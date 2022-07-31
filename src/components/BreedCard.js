1942b61ec1dfe296d488922eb48f1fb1ac9362bf
Amber Suratte <as00193@my.westga.edu>
2022-07-31 12:57:32 -0500

    Adjusted margin, padding, typography styles

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -209,9 +209,9 @@ const BreedCard = () => {
                                     {(breedsList[currentBreedNumber].wikipedia_url || breedsList[currentBreedNumber].vetstreet_url || breedsList[currentBreedNumber].vcahospitals_url) &&
                                         <div className="learn-more section-wrapper">
                                             <h4>Learn More</h4>
-                                            {breedsList[currentBreedNumber].wikipedia_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].wikipedia_url} target="_blank" rel="noopener noreferrer">Wikipedia</a> <span className="external-link"><BoxArrowUpRight /></span></p>}
-                                            {breedsList[currentBreedNumber].vetstreet_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vetstreet_url} target="_blank" rel="noopener noreferrer">Vetstreet</a> <span className="external-link"><BoxArrowUpRight /></span></p>}
-                                            {breedsList[currentBreedNumber].vcahospitals_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vcahospitals_url} target="_blank" rel="noopener noreferrer">VCA Animal Hospitals</a> <span className="external-link"><BoxArrowUpRight /></span></p>}
+                                            {breedsList[currentBreedNumber].wikipedia_url && <p><a className="link-primary mt-4" href={breedsList[currentBreedNumber].wikipedia_url} target="_blank" rel="noopener noreferrer">Wikipedia</a> <span className="external-link"><BoxArrowUpRight /></span></p>}
+                                            {breedsList[currentBreedNumber].vetstreet_url && <p><a className="link-primary mt-4" href={breedsList[currentBreedNumber].vetstreet_url} target="_blank" rel="noopener noreferrer">Vetstreet</a> <span className="external-link"><BoxArrowUpRight /></span></p>}
+                                            {breedsList[currentBreedNumber].vcahospitals_url && <p><a className="link-primary mt-4" href={breedsList[currentBreedNumber].vcahospitals_url} target="_blank" rel="noopener noreferrer">VCA Animal Hospitals</a> <span className="external-link"><BoxArrowUpRight /></span></p>}
                                         </div>
                                     }
                                 </div>
