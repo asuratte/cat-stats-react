bc673eb31a784ac1d7644b844afa69ed69a89821
Amber Suratte <as00193@my.westga.edu>
2022-07-31 11:48:22 -0500

    Added markup and css for desktop layout adjustments

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -86,110 +86,126 @@ const BreedCard = () => {
                                     : <p>Description unavailable</p>
                             }
                         </div>
-                        <div className="image section-wrapper">
-                            {
-                                (breedsList[currentBreedNumber].image && breedsList[currentBreedNumber].image.url)
-                                    ? <img src={breedsList[currentBreedNumber].image.url} alt={breedsList[currentBreedNumber].name} />
-                                    : <img src="https://placekitten.com/300/500" alt={breedsList[currentBreedNumber].name} />
-                            }
-                        </div>
-                        <div className="temperament section-wrapper">
-                            <strong>Temperament</strong>
-                            {
-                                (breedsList[currentBreedNumber].temperament)
-                                    ? <p>{breedsList[currentBreedNumber].temperament}</p>
-                                    : <p>Temperament information unavailable</p>
-                            }
-                        </div>
-                        <div className="origin section-wrapper">
-                            <strong>Place of Origin</strong>
-                            {
-                                (breedsList[currentBreedNumber].origin)
-                                    ? <p>{breedsList[currentBreedNumber].origin}</p>
-                                    : <p>Origin information unavailable</p>
-                            }
-                        </div>
-                        <div className="weight section-wrapper">
-                            <strong>Weight</strong>
-                            {
-                                (breedsList[currentBreedNumber].weight && breedsList[currentBreedNumber].weight.imperial)
-                                    ? <p>{breedsList[currentBreedNumber].weight.imperial} lbs</p>
-                                    : <p>Weight information unavailable</p>
-                            }
-                        </div>
-                        <div className="attributes-points section-wrapper">
-                            <ul>
-                                 {breedsList[currentBreedNumber].affection_level && <li>Affection +{breedsList[currentBreedNumber].affection_level}</li>}
-                                 {breedsList[currentBreedNumber].energy_level && <li>Energy +{breedsList[currentBreedNumber].energy_level}</li>}
-                                 {breedsList[currentBreedNumber].vocalisation && <li>Vocalization +{breedsList[currentBreedNumber].vocalisation}</li>}
-                                 {breedsList[currentBreedNumber].intelligence && <li>Intelligence +{breedsList[currentBreedNumber].intelligence}</li>}
-                                 {breedsList[currentBreedNumber].shedding_level && <li>Shedding +{breedsList[currentBreedNumber].shedding_level}</li>}
-                                 {breedsList[currentBreedNumber].grooming && <li>Grooming +{breedsList[currentBreedNumber].grooming}</li>}
-                                 {breedsList[currentBreedNumber].dog_friendly && <li>Dog Friendly +{breedsList[currentBreedNumber].dog_friendly}</li>}
-                                 {breedsList[currentBreedNumber].child_friendly && <li>Child Friendly +{breedsList[currentBreedNumber].child_friendly}</li>}
-                            </ul>
-                        </div>
-                        <div className="attributes-yes-no section-wrapper">
-                            <ul>
-                                <li>
+                        <div className="two-column-section">
+                            <div className="image-column">
+                                <div className="image section-wrapper">
                                     {
-                                        (breedsList[currentBreedNumber].lap)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
+                                        (breedsList[currentBreedNumber].image && breedsList[currentBreedNumber].image.url)
+                                            ? <img src={breedsList[currentBreedNumber].image.url} alt={breedsList[currentBreedNumber].name} />
+                                            : <img src="https://placekitten.com/300/500" alt={breedsList[currentBreedNumber].name} />
                                     }
-                                    Lap Cat</li>
-                                <li>
-                                    {
-                                        (breedsList[currentBreedNumber].rare)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
-                                    }
-                                    Rare</li>
-                                <li>
-                                    {
-                                        (breedsList[currentBreedNumber].suppressed_tail)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
-                                    }
-                                    Suppressed Tail</li>
-                                <li>
-                                    {
-                                        (breedsList[currentBreedNumber].short_legs)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
-                                    }
-                                    Short Legs</li>
-                                <li>
-                                    {
-                                        (breedsList[currentBreedNumber].hypoallergenic)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
-                                    }
-                                    Hypoallergenic</li>
-                                <li>
-                                    {
-                                        (breedsList[currentBreedNumber].experimental)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
-                                    }
-                                    Experimental</li>
-                                <li>
-                                    {
-                                        (breedsList[currentBreedNumber].natural)
-                                            ? <span className="attribute-icon yes"><CheckLg /></span>
-                                            : <span className="attribute-icon no"><XLg /></span>
-                                    }
-                                    Natural</li>
-                            </ul>
-                        </div>
-                        {(breedsList[currentBreedNumber].wikipedia_url || breedsList[currentBreedNumber].vetstreet_url || breedsList[currentBreedNumber].vcahospitals_url) &&
-                            <div className="learn-more section-wrapper">
-                                <strong>Learn More</strong>
-                                {breedsList[currentBreedNumber].wikipedia_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].wikipedia_url} target="_blank" rel="noopener noreferrer">Wikipedia</a> <BoxArrowUpRight /></p>}
-                                {breedsList[currentBreedNumber].vetstreet_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vetstreet_url} target="_blank" rel="noopener noreferrer">Vetstreet</a> <BoxArrowUpRight /></p>}
-                                {breedsList[currentBreedNumber].vcahospitals_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vcahospitals_url} target="_blank" rel="noopener noreferrer">VCA Animal Hospitals</a> <BoxArrowUpRight /></p>}
+                                </div>
                             </div>
-                        }
+                            <div className="details-column">
+                                <div className="temperament section-wrapper">
+                                    <strong>Temperament</strong>
+                                    {
+                                        (breedsList[currentBreedNumber].temperament)
+                                            ? <p>{breedsList[currentBreedNumber].temperament}</p>
+                                            : <p>Temperament information unavailable</p>
+                                    }
+                                </div>
+                                <div className="three-column-section">
+                                    <div className="lifespan section-wrapper">
+                                        <strong>Life Span</strong>
+                                        {
+                                            (breedsList[currentBreedNumber].life_span)
+                                                ? <p>{breedsList[currentBreedNumber].life_span} years</p>
+                                                : <p>Life span information unavailable</p>
+                                        }
+                                    </div>
+                                    <div className="origin section-wrapper">
+                                        <strong>Place of Origin</strong>
+                                        {
+                                            (breedsList[currentBreedNumber].origin)
+                                                ? <p>{breedsList[currentBreedNumber].origin}</p>
+                                                : <p>Origin information unavailable</p>
+                                        }
+                                    </div>
+                                    <div className="weight section-wrapper">
+                                        <strong>Weight</strong>
+                                        {
+                                            (breedsList[currentBreedNumber].weight && breedsList[currentBreedNumber].weight.imperial)
+                                                ? <p>{breedsList[currentBreedNumber].weight.imperial} lbs</p>
+                                                : <p>Weight information unavailable</p>
+                                        }
+                                    </div>
+                                </div>
+                                <div className="attributes-points section-wrapper">
+                                    <ul>
+                                        {breedsList[currentBreedNumber].affection_level && <li>Affection +{breedsList[currentBreedNumber].affection_level}</li>}
+                                        {breedsList[currentBreedNumber].energy_level && <li>Energy +{breedsList[currentBreedNumber].energy_level}</li>}
+                                        {breedsList[currentBreedNumber].vocalisation && <li>Vocalization +{breedsList[currentBreedNumber].vocalisation}</li>}
+                                        {breedsList[currentBreedNumber].intelligence && <li>Intelligence +{breedsList[currentBreedNumber].intelligence}</li>}
+                                        {breedsList[currentBreedNumber].shedding_level && <li>Shedding +{breedsList[currentBreedNumber].shedding_level}</li>}
+                                        {breedsList[currentBreedNumber].grooming && <li>Grooming +{breedsList[currentBreedNumber].grooming}</li>}
+                                        {breedsList[currentBreedNumber].dog_friendly && <li>Dog Friendly +{breedsList[currentBreedNumber].dog_friendly}</li>}
+                                        {breedsList[currentBreedNumber].child_friendly && <li>Child Friendly +{breedsList[currentBreedNumber].child_friendly}</li>}
+                                    </ul>
+                                </div>
+                                <div className="attributes-yes-no section-wrapper">
+                                    <ul>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].lap)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Lap Cat</li>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].rare)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Rare</li>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].suppressed_tail)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Suppressed Tail</li>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].short_legs)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Short Legs</li>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].hypoallergenic)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Hypoallergenic</li>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].experimental)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Experimental</li>
+                                        <li>
+                                            {
+                                                (breedsList[currentBreedNumber].natural)
+                                                    ? <span className="attribute-icon yes"><CheckLg /></span>
+                                                    : <span className="attribute-icon no"><XLg /></span>
+                                            }
+                                            Natural</li>
+                                    </ul>
+                                </div>
+                                {(breedsList[currentBreedNumber].wikipedia_url || breedsList[currentBreedNumber].vetstreet_url || breedsList[currentBreedNumber].vcahospitals_url) &&
+                                    <div className="learn-more section-wrapper">
+                                        <strong>Learn More</strong>
+                                        {breedsList[currentBreedNumber].wikipedia_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].wikipedia_url} target="_blank" rel="noopener noreferrer">Wikipedia</a> <BoxArrowUpRight /></p>}
+                                        {breedsList[currentBreedNumber].vetstreet_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vetstreet_url} target="_blank" rel="noopener noreferrer">Vetstreet</a> <BoxArrowUpRight /></p>}
+                                        {breedsList[currentBreedNumber].vcahospitals_url && <p><a className="link-primary" href={breedsList[currentBreedNumber].vcahospitals_url} target="_blank" rel="noopener noreferrer">VCA Animal Hospitals</a> <BoxArrowUpRight /></p>}
+                                    </div>
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
